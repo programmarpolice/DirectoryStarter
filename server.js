e278c770b98446c3f9547b5011233fd1c54f7581
Joy Harrington <joysyk23@gmail.com>
2024-10-07 13:34:34 -0500

    Completed

--- server.js
+++ server.js
@@ -14,3 +14,28 @@ const employees = require("./employees");
 app.get("/", (req, res) => {
   res.send("Hello employees!");
 });
+
+app.get("/employees", (req, res) => {
+  res.json(employees);
+});
+
+app.get("/employees/random", (req, res) => {
+  const randomIndex = Math.floor(Math.random() * employees.length);
+  console.log(randomIndex);
+  const randomEmployee = employees[randomIndex];
+
+  res.send(randomEmployee.name);
+});
+
+app.get("/employees/:id", (req, res) => {
+  const employee = employees.find((obj) => obj.id === +req.params.id);
+  if (employee) {
+    res.send(employee.name);
+  } else {
+    res.send("id not located");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}.`);
+});
